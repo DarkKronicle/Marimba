3b5727799c8615de87c2cc54a7ba7f8e2562e3bc
DarkKronicle <darkkronicle@gmail.com>
2021-11-18 15:57:18 -0700

    Multiple prefixes

--- bot/marimba_bot.py
+++ bot/marimba_bot.py
@@ -28,18 +28,19 @@ async def get_prefix(bot_obj, message: discord.Message):
     prefixes = ['x>', '<@!{0}> '.format(user_id)]
     space = ['x> ', '<@!{0}> '.format(user_id)]
     if message.guild is None:
-        prefix = '>'
+        custom_prefix = ['>']
     else:
-        prefix = await bot_obj.get_guild_prefix(message.guild)
-        if prefix is None:
-            prefix = '>'
+        custom_prefix = await bot_obj.get_guild_prefixes(message.guild)
+        if custom_prefix is None:
+            custom_prefix = ['>']
     message_content: str = message.content
     if message_content.startswith('x> '):
         return space
-    if message_content.startswith('{0} '.format(prefix)):
-        space.append('{0} '.format(prefix))
-        return space
-    prefixes.append(prefix)
+    for prefix in custom_prefix:
+        if message_content.startswith('{0} '.format(prefix)):
+            space.append('{0} '.format(prefix))
+            return space
+    prefixes.extend(custom_prefix)
     return prefixes
 
 
@@ -71,11 +72,15 @@ class MarimbaBot(bot.Bot):
                 logging.warning('Failed to load extension {0}.'.format(extension))
                 traceback.print_exc()
 
-    async def get_guild_prefix(self, guild):
+    async def get_guild_prefixes(self, guild):
         settings = await guild_config.get_guild_settings(self, guild)
         if not settings:
-            return '~'
-        return settings.prefix
+            return ['>']
+        return settings.prefix.split(',')
+
+    async def get_guild_prefix(self, guild):
+        prefixes = await self.get_guild_prefixes(guild)
+        return ', '.join(prefixes)
 
     async def on_ready(self):
         logging.info('Bot up and running!')
@@ -92,6 +97,7 @@ class MarimbaBot(bot.Bot):
             await super().on_command_error(ctx, error, raise_err=raise_err)
         except Exception as new_exception:
             if isinstance(new_exception, errors.MissingRequiredArgument):
-                await ctx.send(embed=ctx.create_embed(description="Missing argument!", error=True), delete_after=5)
+                return await ctx.send(embed=ctx.create_embed(description="Missing argument!", error=True), delete_after=5)
+            raise new_exception
 
 
